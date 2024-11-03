0708fb6eff90b39538e0f03fd537f3a941cd0888
Ryan McManamy <ramcmana@asu.edu>
2024-11-02 17:52:14 -0700

    Moved again

--- satellite_experience/assets/js/main.js
+++ satellite_experience/assets/js/main.js
@@ -394,16 +394,17 @@ document.addEventListener("DOMContentLoaded", function() {
     setTimeout(function() {
         loading.style.opacity = 0;
         setTimeout(function() {
+            var iframe = document.createElement('iframe');
+            var html = '<body>Foo</body>';
+            //iframe.src = "../pages/main_page.html";
+            document.body.appendChild(iframe);
+            iframe.contentWindow.document.open();
+            iframe.contentWindow.document.write(html);
+            iframe.contentWindow.document.close();
+            iframe.style = "position: fixed; top: 0px;bottom: 0px; right: 0px; width: 100%; border: none; margin: 0; padding: 0; overflow: hidden; z-index: 999999; height: 100%;";
+            iframe.src = "../pages/main_page.html";
+            
             loading.style.display = "none";
-                var iframe = document.createElement('iframe');
-                var html = '<body>Foo</body>';
-                //iframe.src = "../pages/main_page.html";
-                document.body.appendChild(iframe);
-                iframe.contentWindow.document.open();
-                iframe.contentWindow.document.write(html);
-                iframe.contentWindow.document.close();
-                iframe.style = "position: fixed; top: 0px;bottom: 0px; right: 0px; width: 100%; border: none; margin: 0; padding: 0; overflow: hidden; z-index: 999999; height: 100%;";
-                iframe.src = "../pages/main_page.html";
         }, 3000);
     }, 2000);
 
