4d21177715a4b619397a48634f8efe3aa48d117b
Ryan McManamy <ramcmana@asu.edu>
2024-11-07 17:48:57 -0700

    Trying with parentheses

--- satellite_experience/assets/js/index.js
+++ satellite_experience/assets/js/index.js
@@ -36,7 +36,8 @@ function onClick() {
 	// audio.play();
   //window.location.href = "main_page.html";
 
-    var iframe = document.createElement('iframe');
+    //var iframe = document.createElement('iframe');
+    let iframe = document.createElement('iframe');
     iframe.style = "position: fixed; top: 0px;bottom: 0px; right: 0px; width: 100%; border: none; margin: 0; padding: 0; overflow: hidden; z-index: 999999; height: 100%;";
 	iframe.src = "../pages/main_page.html";
     //iframe.id = "iframe_a";
