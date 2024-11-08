8809f863ff97ae05ddc8174572a6fba8c8c8174e
Ryan McManamy <ramcmana@asu.edu>
2024-11-07 18:26:15 -0700

    Used window.parent.window.samples

--- satellite_experience/assets/js/main.js
+++ satellite_experience/assets/js/main.js
@@ -5,6 +5,8 @@ import * as THREE from 'https://cdn.jsdelivr.net/npm/three@0.118/build/three.mod
 import { OrbitControls } from 'https://cdn.jsdelivr.net/npm/three@0.118/examples/jsm/controls/OrbitControls.js';
 import { GLTFLoader } from 'https://cdn.jsdelivr.net/npm/three@0.118/examples/jsm/loaders/GLTFLoader.js';
 
+let iframeSamples = window.parent.window.samples;
+
 class SpaceSkybox {
     constructor() {
         this._Initialize();
@@ -224,7 +226,8 @@ document.addEventListener("DOMContentLoaded", function() {
     var helpIconButton = document.getElementById("help-icon-button");
     helpIconButton.addEventListener("click", function() {
 //        sound.src = "../audio/open.mp3";
-        window.parent.playSample((window.parent.samples)[1], 0);
+        //window.parent.playSample((window.parent.samples)[1], 0);
+        window.parent.playSample(iframeSamples[1], 0);
         //parent.playSample(parent.samples[1], 0);
         //document.getElementById('sound').play();
         openHelpModal();
@@ -234,7 +237,8 @@ document.addEventListener("DOMContentLoaded", function() {
     var helpModalCloseButton = document.getElementById("help-modal-close");
     helpModalCloseButton.addEventListener("click", function() {
 //        sound.src = "../audio/close.mp3";
-        window.parent.playSample((window.parent.samples)[3], 0);
+        //window.parent.playSample((window.parent.samples)[3], 0);
+        window.parent.playSample(iframeSamples[3], 0);
         //parent.playSample(parent.samples[3], 0);
         //document.getElementById('sound').play();
         helpModal.style.display = "none";
@@ -307,7 +311,8 @@ document.addEventListener("DOMContentLoaded", function() {
                 radioSetting.forEach(radio => {
                     radio.addEventListener("change", function() {
 //                        sound.src = "../audio/select.mp3";
-                        window.parent.playSample((window.parent.samples)[2], 0);
+                        //window.parent.playSample((window.parent.samples)[2], 0);
+                        window.parent.playSample(iframeSamples[2], 0);
                         //parent.playSample(parent.samples[2], 0);
                         if (document.getElementById('default-mode').checked) {
                             //sound.src = "../audio/select.mp3";
@@ -338,7 +343,8 @@ document.addEventListener("DOMContentLoaded", function() {
     var settingsIconButton = document.getElementById("settings-icon-button");
     settingsIconButton.addEventListener("click", function() {
 //        sound.src = "../audio/open.mp3";
-        window.parent.playSample((window.parent.samples)[1], 0);
+        //window.parent.playSample((window.parent.samples)[1], 0);
+        window.parent.playSample(iframeSamples[1], 0);
         //parent.playSample(parent.samples[1], 0);
         openSettingsModal();
     });
@@ -348,7 +354,8 @@ document.addEventListener("DOMContentLoaded", function() {
     settingsModalCloseButton.addEventListener("click", function() {
         resetInactivityTimer(); // Reset activity timer so it doesn't pop up as soon as settings is closed
 //        sound.src = "../audio/close.mp3";
-        window.parent.playSample((window.parent.samples)[3], 0);
+        //window.parent.playSample((window.parent.samples)[3], 0);
+        window.parent.playSample(iframeSamples[3], 0);
         //parent.playSample(parent.samples[3], 0);
         settingsModal.style.display = "none";
     });
