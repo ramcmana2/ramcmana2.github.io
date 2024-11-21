a370da744c1159cabb02908acf07268d3102eb60
Ryan McManamy <ramcmana@asu.edu>
2024-11-20 22:36:58 -0700

    Set pos to (0, 20, -39)

--- satellite_experience/assets/js/SpaceScene.js
+++ satellite_experience/assets/js/SpaceScene.js
@@ -389,7 +389,8 @@ export default class SpaceScene {
         //instructionsGroup.position.set(0, 16, -40);
         //instructionsGroup.position.set(0, 19, -39);
         //instructionsGroup.position.set(0, 3, -39);
-        instructionsGroup.position.set(0, 24, -39);
+        //instructionsGroup.position.set(0, 24, -39);
+        instructionsGroup.position.set(0, 20, -39);
 
         // Create instructions label
         const instructionsLabelDiv = document.createElement('div');
