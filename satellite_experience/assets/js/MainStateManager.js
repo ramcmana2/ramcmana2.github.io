30a89e7434b8d764cd52ea9f5897c695c08608b0
Ryan McManamy <ramcmana@asu.edu>
2024-11-22 20:25:12 -0700

    Hardcode the time in milliseconds

--- satellite_experience/assets/js/MainStateManager.js
+++ satellite_experience/assets/js/MainStateManager.js
@@ -38,7 +38,8 @@ function getCurrentDateFormatted() {
 }
 
 //var missionCompletionTime = new Date("2029-Jun-16").getTime();
-var missionCompletionTime = new Date("2029-Jun-16T00:00:00.000").getTime();
+//var missionCompletionTime = new Date("2029-Jun-16T00:00:00.000").getTime();
+var missionCompletionTime = 1876262400000;
 //console.log("" + missionCompletionTime);
 var x = setInterval(function() {
   //var currentDate = new Date().getTime();
