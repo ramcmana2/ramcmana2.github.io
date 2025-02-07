7347671af94a8d6b80a6a54aef92071104612748
Ryan McManamy <ramcmana@asu.edu>
2025-02-06 19:56:31 -0700

    This should work

--- lore_and_mythology_experience/assets/js/main.js
+++ lore_and_mythology_experience/assets/js/main.js
@@ -159,12 +159,12 @@ function showAnnibale() {
     //calc(((100vh - 40vw) / 2) + (0.5 * 100vh) - (0.5 * 40vw))
     //calc((100vw - 40vw) / 2)
 
-    document.getElementById("annibale").setAttribute("style", "background-color: transparent; width: calc(0.8 * 40vh); height: 40vh; border-radius: 12px; padding: 5vh; position: absolute; top: 0; left: calc(50vw - ((0.8 * 40vh) / 2);");
-    document.getElementById("papyrus_double_sided").setAttribute("style", "background-color: transparent; width: 40vw; height: 40vw; border-radius: 12px; position: absolute; top: 50vh; left: calc(50vw - (40vh / 2));");
-    document.getElementById("scroll_text_box").setAttribute("style", "border-style: solid; border-width: thin; border-color: black; border-radius: 5px; padding: 1vw; display: flex; flex-direction: column; position: absolute; top: calc(50vh + (40vh / 2)); left: calc(((100vw - 40vw) / 2) + ((40vw / 2) / 2));");
+    document.getElementById("annibale").setAttribute("style", "background-color: transparent; width: calc(0.8 * 40vh); height: 40vh; border-radius: 12px; padding: 5vh; position: absolute; top: 0; left: calc(50vw - ((0.8 * 50vh) / 2));");
+    document.getElementById("papyrus_double_sided").setAttribute("style", "background-color: transparent; width: 40vh; height: 40vh; border-radius: 12px; position: absolute; top: 50vh; left: calc(50vw - (40vh / 2));");
+    document.getElementById("scroll_text_box").setAttribute("style", "display: flex; flex-direction: column; position: absolute; width: 40vh; height: calc(40vh / 2); top: calc(50vh + ((40vh / 1.7) / 1.7)); left: calc(50vw - (40vh / 2));");
     var infos = document.getElementsByClassName("info");
     for (var i = 0; i < infos.length; i++) {
-        infos[i].setAttribute("style", "text-align: center; font-size: calc(0.02 * 0.75 * 80vw); z-index: 21;");
+        infos[i].setAttribute("style", "text-align: center; font-size: calc(0.05 * 40vh); z-index: 21;");
     }
 }
 
