ecb9e81bec601fd788fc31784d288858f77e9bd1
Ryan McManamy <ramcmana@asu.edu>
2025-02-06 21:17:22 -0700

    Black background

--- lore_and_mythology_experience/assets/js/main.js
+++ lore_and_mythology_experience/assets/js/main.js
@@ -129,7 +129,7 @@ function showAnnibale() {
     if (!annibool) {
         const annibale_div = document.createElement("div");
         annibale_div.setAttribute("id", "annibale_modal");
-        annibale_div.setAttribute("style", "display: 'block'; position: fixed; z-index: 20; left: 0; top: 0; width: 100%; height: 100%; background-color: rgba(0, 0, 0, 0.5);");
+        annibale_div.setAttribute("style", "display: 'block'; position: fixed; z-index: 20; left: 0; top: 0; width: 100%; height: 100%; background-color: rgba(0, 0, 0, 1);");
 
         let annibale_innerHTML = "";
         annibale_innerHTML += `<img src="../assets/images/annibale.jpg" id="annibale"/>
@@ -166,7 +166,7 @@ function showAnnibale() {
 
         document.getElementById("annibale").setAttribute("style", "background-color: transparent; width: calc(0.8 * 40vh); height: 40vh; border-radius: 12px; padding: 5vh; position: absolute; top: calc(0.25 * 40vh); left: calc(50vw - ((0.8 * 40vh + 10vh) / 2));");
         document.getElementById("papyrus_double_sided").setAttribute("style", "background-color: transparent; width: 40vh; height: 40vh; border-radius: 12px; position: absolute; top: 50vh; left: calc(50vw - (40vh / 2));");
-        document.getElementById("scroll_text_box").setAttribute("style", "display: flex; flex-direction: column; position: absolute; width: 40vh; height: calc(40vh / 2); top: calc(50vh + ((40vh / 1.7) / 1.7)); left: calc(50vw - (40vh / 2));");
+        document.getElementById("scroll_text_box").setAttribute("style", "display: flex; flex-direction: column; position: absolute; width: 40vh; height: calc(40vh / 2); top: calc(50vh + ((40vh / 1.69) / 1.69)); left: calc(50vw - (40vh / 2));");
         var infos = document.getElementsByClassName("info");
         for (var i = 0; i < infos.length; i++) {
             infos[i].setAttribute("style", "text-align: center; font-size: calc(0.045 * 40vh); z-index: 21;");
@@ -174,10 +174,10 @@ function showAnnibale() {
         annibool = true;
     }
     else {
-        document.getElementById("annibale_modal").setAttribute("style", "display: 'block'; position: fixed; z-index: 20; left: 0; top: 0; width: 100%; height: 100%; background-color: rgba(0, 0, 0, 0.5);");
+        document.getElementById("annibale_modal").setAttribute("style", "display: 'block'; position: fixed; z-index: 20; left: 0; top: 0; width: 100%; height: 100%; background-color: rgba(0, 0, 0, 1);");
         document.getElementById("annibale").setAttribute("style", "background-color: transparent; width: calc(0.8 * 40vh); height: 40vh; border-radius: 12px; padding: 5vh; position: absolute; top: calc(0.25 * 40vh); left: calc(50vw - ((0.8 * 40vh + 10vh) / 2));");
         document.getElementById("papyrus_double_sided").setAttribute("style", "background-color: transparent; width: 40vh; height: 40vh; border-radius: 12px; position: absolute; top: 50vh; left: calc(50vw - (40vh / 2));");
-        document.getElementById("scroll_text_box").setAttribute("style", "display: flex; flex-direction: column; position: absolute; width: 40vh; height: calc(40vh / 2); top: calc(50vh + ((40vh / 1.7) / 1.7)); left: calc(50vw - (40vh / 2));");
+        document.getElementById("scroll_text_box").setAttribute("style", "display: flex; flex-direction: column; position: absolute; width: 40vh; height: calc(40vh / 2); top: calc(50vh + ((40vh / 1.69) / 1.69)); left: calc(50vw - (40vh / 2));");
     }
 }
 
