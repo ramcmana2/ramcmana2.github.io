945ad3345148c63c0149b763e7466be8cb11c684
Ryan McManamy <ramcmana@asu.edu>
2025-02-06 20:50:54 -0700

    Only annibale once

--- lore_and_mythology_experience/assets/js/main.js
+++ lore_and_mythology_experience/assets/js/main.js
@@ -121,50 +121,57 @@ function moveScope(event) {
     }
 }
 
+let annibool = false;
 function showAnnibale() {
-    const annibale_div = document.createElement("div");
-    annibale_div.setAttribute("id", "annibale_modal");
-    annibale_div.setAttribute("style", "display: 'block'; position: fixed; z-index: 20; left: 0; top: 0; width: 100%; height: 100%; background-color: rgba(0, 0, 0, 0.5);");
+    if (!annibool) {
+        const annibale_div = document.createElement("div");
+        annibale_div.setAttribute("id", "annibale_modal");
+        annibale_div.setAttribute("style", "display: 'block'; position: fixed; z-index: 20; left: 0; top: 0; width: 100%; height: 100%; background-color: rgba(0, 0, 0, 0.5);");
 
-    let annibale_innerHTML = "";
-    annibale_innerHTML += `<img src="../assets/images/annibale.jpg" id="annibale"/>
-                           <img src="../assets/images/papyrus_scroll_double_sided.png" id="papyrus_double_sided"/>
-                           <div id="scroll_text_box">
-                             <span class="info">On March 17, 1852, the Italian astronomer</span>
-                             <span class="info">Annibale de Gasparis discovered the 16th</span>
-                             <span class="info">asteroid in the main asteroid belt</span>
-                             <span class="info">between Mars and Jupiter.</span>
-                             <span class="info">De Gasparis named this asteroid Psyche.</span>
-                           </div>`;
-    annibale_div.innerHTML = annibale_innerHTML;
-    document.body.appendChild(annibale_div);
+        let annibale_innerHTML = "";
+        annibale_innerHTML += `<img src="../assets/images/annibale.jpg" id="annibale"/>
+                               <img src="../assets/images/papyrus_scroll_double_sided.png" id="papyrus_double_sided"/>
+                               <div id="scroll_text_box">
+                                 <span class="info">On March 17, 1852, the Italian astronomer</span>
+                                 <span class="info">Annibale de Gasparis discovered the 16th</span>
+                                 <span class="info">asteroid in the main asteroid belt</span>
+                                 <span class="info">between Mars and Jupiter.</span>
+                                 <span class="info">De Gasparis named this asteroid Psyche.</span>
+                               </div>`;
+        annibale_div.innerHTML = annibale_innerHTML;
+        document.body.appendChild(annibale_div);
 
-    //document.getElementById("annibale").setAttribute("style", "background-color: transparent; width: 50vw; height: calc(1.25 * 50vw); border-radius: 12px; position: relative; top: calc(((100vh - 1.25 * 50vw) / 2) / 2); left: calc((100vw - 50vw) / 2);");
-    //document.getElementById("papyrus_double_sided").setAttribute("style", "background-color: transparent; width: 50vw; height: calc(1.25 * 50vw); border-radius: 12px; position: relative; top: calc(((100vh - 1.25 * 50vw) / 2) + (((100vh - 1.25 * 50vw) / 2) / 2)); left: calc((100vw - 50vw) / 2);");
-    //document.getElementById("scroll_text_box").setAttribute("style", "border-style: solid; border-width: thin; border-color: black; border-radius: 5px; padding: 1vw; display: flex; flex-direction: column;");
-    //document.getElementsByClassName("info").setAttribute("style", "text-align: center; font-size: calc(0.02 * 0.75 * 80vw);");
+        //document.getElementById("annibale").setAttribute("style", "background-color: transparent; width: 50vw; height: calc(1.25 * 50vw); border-radius: 12px; position: relative; top: calc(((100vh - 1.25 * 50vw) / 2) / 2); left: calc((100vw - 50vw) / 2);");
+        //document.getElementById("papyrus_double_sided").setAttribute("style", "background-color: transparent; width: 50vw; height: calc(1.25 * 50vw); border-radius: 12px; position: relative; top: calc(((100vh - 1.25 * 50vw) / 2) + (((100vh - 1.25 * 50vw) / 2) / 2)); left: calc((100vw - 50vw) / 2);");
+        //document.getElementById("scroll_text_box").setAttribute("style", "border-style: solid; border-width: thin; border-color: black; border-radius: 5px; padding: 1vw; display: flex; flex-direction: column;");
+        //document.getElementsByClassName("info").setAttribute("style", "text-align: center; font-size: calc(0.02 * 0.75 * 80vw);");
 
-    // document.getElementById("annibale").setAttribute("style", "background-color: transparent; width: calc(0.8 * 40vh); height: 40vh; border-radius: 12px; position: relative; top: calc(((100vh - 40vh) / 2) - (((100vh - 40vh) / 2) / 2)); left: calc((100vw - 0.8 * 40vh) / 2);");
-    // document.getElementById("papyrus_double_sided").setAttribute("style", "background-color: transparent; width: 40vw; height: 40vw; border-radius: 12px; position: relative; top: calc(((100vh - 40vw) / 2) / 2); left: calc((100vw - 40vw) / 2);");
-    // document.getElementById("scroll_text_box").setAttribute("style", "border-style: solid; border-width: thin; border-color: black; border-radius: 5px; padding: 1vw; display: flex; flex-direction: column;");
+        // document.getElementById("annibale").setAttribute("style", "background-color: transparent; width: calc(0.8 * 40vh); height: 40vh; border-radius: 12px; position: relative; top: calc(((100vh - 40vh) / 2) - (((100vh - 40vh) / 2) / 2)); left: calc((100vw - 0.8 * 40vh) / 2);");
+        // document.getElementById("papyrus_double_sided").setAttribute("style", "background-color: transparent; width: 40vw; height: 40vw; border-radius: 12px; position: relative; top: calc(((100vh - 40vw) / 2) / 2); left: calc((100vw - 40vw) / 2);");
+        // document.getElementById("scroll_text_box").setAttribute("style", "border-style: solid; border-width: thin; border-color: black; border-radius: 5px; padding: 1vw; display: flex; flex-direction: column;");
 
-    // document.getElementById("annibale").setAttribute("style", "background-color: transparent; width: calc(0.8 * 40vh); height: 40vh; border-radius: 12px; position: relative; top: calc(((100vh - 40vh) / 2) - (((100vh - 40vh) / 2) / 2)); left: calc((100vw - 0.8 * 40vh) / 2);");
-    // document.getElementById("papyrus_double_sided").setAttribute("style", "background-color: transparent; width: 40vw; height: 40vw; border-radius: 12px; position: absolute; top: 50vh; left: calc((100vw - 40vw) / 2);");
-    // document.getElementById("scroll_text_box").setAttribute("style", "border-style: solid; border-width: thin; border-color: black; border-radius: 5px; padding: 1vw; display: flex; flex-direction: column; position: absolute; top: calc(50vh + (40vh / 2)); left: calc(((100vw - 40vw) / 2) + ((40vw / 2) / 2));");
-    // var infos = document.getElementsByClassName("info");
-    // for (var i = 0; i < infos.length; i++) {
-    //     infos[i].setAttribute("style", "text-align: center; font-size: calc(0.02 * 0.75 * 80vw); z-index: 21;");
-    // }
+        // document.getElementById("annibale").setAttribute("style", "background-color: transparent; width: calc(0.8 * 40vh); height: 40vh; border-radius: 12px; position: relative; top: calc(((100vh - 40vh) / 2) - (((100vh - 40vh) / 2) / 2)); left: calc((100vw - 0.8 * 40vh) / 2);");
+        // document.getElementById("papyrus_double_sided").setAttribute("style", "background-color: transparent; width: 40vw; height: 40vw; border-radius: 12px; position: absolute; top: 50vh; left: calc((100vw - 40vw) / 2);");
+        // document.getElementById("scroll_text_box").setAttribute("style", "border-style: solid; border-width: thin; border-color: black; border-radius: 5px; padding: 1vw; display: flex; flex-direction: column; position: absolute; top: calc(50vh + (40vh / 2)); left: calc(((100vw - 40vw) / 2) + ((40vw / 2) / 2));");
+        // var infos = document.getElementsByClassName("info");
+        // for (var i = 0; i < infos.length; i++) {
+        //     infos[i].setAttribute("style", "text-align: center; font-size: calc(0.02 * 0.75 * 80vw); z-index: 21;");
+        // }
 
-    //calc(((100vh - 40vw) / 2) + (0.5 * 100vh) - (0.5 * 40vw))
-    //calc((100vw - 40vw) / 2)
+        //calc(((100vh - 40vw) / 2) + (0.5 * 100vh) - (0.5 * 40vw))
+        //calc((100vw - 40vw) / 2)
 
-    document.getElementById("annibale").setAttribute("style", "background-color: transparent; width: calc(0.8 * 40vh); height: 40vh; border-radius: 12px; padding: 5vh; position: absolute; top: calc(0.25 * 40vh); left: calc(50vw - ((0.8 * 40vh + 10vh) / 2));");
-    document.getElementById("papyrus_double_sided").setAttribute("style", "background-color: transparent; width: 40vh; height: 40vh; border-radius: 12px; position: absolute; top: 50vh; left: calc(50vw - (40vh / 2));");
-    document.getElementById("scroll_text_box").setAttribute("style", "display: flex; flex-direction: column; position: absolute; width: 40vh; height: calc(40vh / 2); top: calc(50vh + ((40vh / 1.7) / 1.7)); left: calc(50vw - (40vh / 2));");
-    var infos = document.getElementsByClassName("info");
-    for (var i = 0; i < infos.length; i++) {
-        infos[i].setAttribute("style", "text-align: center; font-size: calc(0.045 * 40vh); z-index: 21;");
+        document.getElementById("annibale").setAttribute("style", "background-color: transparent; width: calc(0.8 * 40vh); height: 40vh; border-radius: 12px; padding: 5vh; position: absolute; top: calc(0.25 * 40vh); left: calc(50vw - ((0.8 * 40vh + 10vh) / 2));");
+        document.getElementById("papyrus_double_sided").setAttribute("style", "background-color: transparent; width: 40vh; height: 40vh; border-radius: 12px; position: absolute; top: 50vh; left: calc(50vw - (40vh / 2));");
+        document.getElementById("scroll_text_box").setAttribute("style", "display: flex; flex-direction: column; position: absolute; width: 40vh; height: calc(40vh / 2); top: calc(50vh + ((40vh / 1.7) / 1.7)); left: calc(50vw - (40vh / 2));");
+        var infos = document.getElementsByClassName("info");
+        for (var i = 0; i < infos.length; i++) {
+            infos[i].setAttribute("style", "text-align: center; font-size: calc(0.045 * 40vh); z-index: 21;");
+        }
+        anniool = true;
+    }
+    else {
+        document.getElementById("annibale_modal").setAttribute("style", "display: 'block'");
     }
 }
 
