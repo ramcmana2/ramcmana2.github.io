ba3f29f54881a954334e26b4d6bd1083d2b84c61
Ryan McManamy <ramcmana@asu.edu>
2025-02-17 14:27:20 -0700

    Also checking overlay ids removal

--- lore_and_mythology_experience/assets/js/phases.js
+++ lore_and_mythology_experience/assets/js/phases.js
@@ -298,7 +298,8 @@ function showPhase(phase) {
 
             // remove any overlay images for the phase
             const overlayImages = document.querySelectorAll(
-                '[id^="chrysalis"], [id^="butterfly"], [id^="chrysalis2"], [id^="butterfly2"]');
+                '[id^="chrysalis"], [id^="butterfly"]');
+                //'[id^="chrysalis"], [id^="butterfly"], [id^="chrysalis2"], [id^="butterfly2"]');
             overlayImages.forEach((img) => img.remove());
 
             phaseBool = false;
