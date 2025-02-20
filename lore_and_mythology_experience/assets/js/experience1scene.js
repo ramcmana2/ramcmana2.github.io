5caa3c5a5344a6f0f3edad925a43d6a6abeee2ed
Ryan McManamy <ramcmana@asu.edu>
2025-02-20 03:39:38 -0700

    80, 8, 105

--- lore_and_mythology_experience/assets/js/experience1scene.js
+++ lore_and_mythology_experience/assets/js/experience1scene.js
@@ -145,9 +145,9 @@ export function experience1() {
         // model.rotateOnAxis(new THREE.Vector3(1, 0, 0), Math.PI / 90);
         // model.rotateOnAxis(new THREE.Vector3(0, 0, 1), Math.PI / 8);
         // model.rotateOnAxis(new THREE.Vector3(0, 1, 0), Math.PI / 6);
-        model.rotateOnWorldAxis(new THREE.Vector3(1, 0, 0), Math.PI / 90);
+        model.rotateOnWorldAxis(new THREE.Vector3(1, 0, 0), Math.PI / 80);
         model.rotateOnWorldAxis(new THREE.Vector3(0, 0, 1), Math.PI / 8);
-        model.rotateOnWorldAxis(new THREE.Vector3(0, 1, 0), Math.PI / 120);
+        model.rotateOnWorldAxis(new THREE.Vector3(0, 1, 0), Math.PI / 105);
         _scene.add(model); // Add model to scene
         //let _model = model;
 
