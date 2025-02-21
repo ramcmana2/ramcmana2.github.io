fedec4acf2dcd91c81e6e8d9e8ab353492a0c97b
Ryan McManamy <ramcmana@asu.edu>
2025-02-20 19:54:46 -0700

    x - 10, will mobile ever display smaller?

--- lore_and_mythology_experience/assets/js/experience1scene.js
+++ lore_and_mythology_experience/assets/js/experience1scene.js
@@ -97,7 +97,7 @@ export function experience1() {
     const x = radius * Math.sin(phi) * Math.cos(theta);
     const y = radius * Math.cos(phi);
     const z = radius * Math.sin(phi) * Math.sin(theta);
-    _camera.position.set(x - 3, y, z);
+    _camera.position.set(x - 10, y, z);
     _camera.lookAt(0, 0, 0);
     //_camera.rotateOnAxis(new THREE.Vector3(0, 1, 0), Math.PI / 12);
 
