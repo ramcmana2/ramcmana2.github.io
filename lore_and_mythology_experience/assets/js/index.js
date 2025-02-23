84e48957a6ee9b31428454ad0b64f373264eccfc
Ryan McManamy <ramcmana@asu.edu>
2025-02-22 23:22:55 -0700

    I think this was the only problem

--- lore_and_mythology_experience/assets/js/index.js
+++ lore_and_mythology_experience/assets/js/index.js
@@ -1,7 +1,7 @@
 function onClick() {
     let iframe = document.createElement('iframe');
     iframe.style = "position: fixed; top: 0px; bottom: 0px; right: 0px; width: 100%; border: none; margin: 0; padding: 0; overflow: hidden; z-index: 999999; height: 100%;";
-    iframe.src = "../pages/main_page.html";
+    iframe.src = "../pages/amp_context.html";
     document.body.appendChild(iframe);
 }
 
