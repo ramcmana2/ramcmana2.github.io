4a0c3d3cd711fbdee1e97f6886f59270f154b5ad
Ryan McManamy <ramcmana@asu.edu>
2025-03-02 22:05:34 -0700

    Slightly larger, and fixed string + int

--- lore_and_mythology_experience/assets/js/phasesSMP.js
+++ lore_and_mythology_experience/assets/js/phasesSMP.js
@@ -362,7 +362,12 @@ function showTimer(callback) {
     }
 
     for (let i = 0; i < 20; i++) {
+        arrivalCountdown["seconds"] = arrivalCountdown["seconds"] + (arrivalIncrement * i);
         arrivalCountdown["seconds"] = leadingZeros(arrivalCountdown["seconds"]);
+
+        completionCountdown["seconds"] = completionCountdown["seconds"] + (completionIncrement * i);
+        completionCountdown["seconds"] = leadingZeros(completionCountdown["seconds"]);
+
         let countdown = {
             placeholder: {
                 image: "",
@@ -378,7 +383,7 @@ function showTimer(callback) {
                     ("" + "Days    | Since Launch |      " + arrivalCountdown["days"] + "      |        " + completionCountdown["days"] + "         |"),
                     ("" + "Hours   | Since Launch |       " + arrivalCountdown["hours"] + "      |         " + completionCountdown["hours"] + "         |"),
                     ("" + "Minutes | Since Launch |       " + arrivalCountdown["minutes"] + "      |         " + completionCountdown["minutes"] + "         |"),
-                    ("" + "Seconds | Since Launch |       " + (arrivalCountdown["seconds"] + (arrivalIncrement * i)) + "      |         " + (completionCountdown["seconds"] + (completionIncrement * i)) + "         |")
+                    ("" + "Seconds | Since Launch |       " + arrivalCountdown["seconds"] + "      |         " + completionCountdown["seconds"] + "         |")
                 ]
             }
         }
@@ -416,6 +421,86 @@ function leadingZeros(number, days=false) {
         }
         return stringNum;
     }
+}
+
+function showCountdown(phase, count) {
+    console.log('Transitioning to countdown phase');
+
+    if (count == 0) {
+        // set up html and css
+        const phase_div = document.createElement("div");
+        phase_div.setAttribute("id", "phase_modal");
+        phase_div.setAttribute("style", "display: block; position: fixed;" +
+            " z-index: 20; left: 0; top: 0; width: 100%; height: 100%; " +
+            "background-color: rgba(0, 0, 0, 0.2); overflow: hidden; transition: 1.5s; font-size: 16px");
+
+        let phase_innerHTML = "";
+
+        phase_innerHTML += `<img src="${phase.banner}" id="banner"/>`;
+
+        if (phase.text.some(line => line !== "")) {
+            phase_innerHTML += `<div id="banner_text_box">`;
+            phase.text.forEach((line) => {
+                phase_innerHTML += `<span class="info">${line}</span>`;
+            });
+            phase_innerHTML += `</div>`;
+        }
+
+        phase_innerHTML += ``;
+        phase_div.innerHTML = phase_innerHTML;
+        document.body.appendChild(phase_div);
+    }
+    else {
+        let phase_innerHTML = "";
+
+        phase_innerHTML += `<img src="${phase.banner}" id="banner"/>`;
+
+        if (phase.text.some(line => line !== "")) {
+            phase_innerHTML += `<div id="banner_text_box">`;
+            phase.text.forEach((line) => {
+                phase_innerHTML += `<span class="info">${line}</span>`;
+            });
+            phase_innerHTML += `</div>`;
+        }
+
+        phase_innerHTML += ``;
+
+        document.getElementById("phase_modal").innerHTML = phase_innerHTML;
+    }
+
+    document.getElementById("banner").setAttribute("style",
+        "background-color: transparent; width: calc(30vw + 15vh); height: auto; border-radius: 12px;" +
+        " position: absolute; top: 70%; left: 50%;" +
+        " z-index: 5; transition: 1.5s ease-in-out; transform: translate(-50%, -50%);");
+
+    if (phase.text.some(line => line !== "")) {
+        const text = document.getElementById("banner_text_box");
+        text.setAttribute("style", " display: flex; flex-direction: column; position: absolute;" +
+            " top: 80%; left: 43%; transform: translate(-50%, -50%);" +
+            " color: #C9FFFC; font-size: 2rem; font-family: 'Comfortaa', Arial, sans-serif; text-align: center;" +
+            " z-index: 10; padding: 10px 20px; border-radius: 8px; transform: translate(-50%, -50%);");
+    }
+
+    var infos = document.getElementsByClassName("info");
+    for (var i = 0; i < infos.length; i++) {
+        // infos[i].setAttribute("style", "text-align: center; font-size: calc(0.045 * 40vh);" +
+        //     " z-index: 21; transition: 1.5s east-in;");
+        infos[i].setAttribute("style", "text-align: center; font-size: calc(0.04 * 40vh);" +
+            " z-index: 21; transition: 1.5s east-in;");
+    }
+
+    // clear phase after 20 seconds
+    if (count == 19) {
+        setTimeout(() => {
+            document.getElementById("phase_modal").remove();
+        }, 1000);
+    }
+
+// else {
+//     // Hide the current phase modal if it's already showing
+//     document.getElementById("phase_modal").setAttribute("style", "display: none;");
+//     phaseBool = false;
+// }
 }
 
 /*
@@ -544,84 +629,4 @@ function showPhase(phase) {
         document.getElementById("phase_modal").setAttribute("style", "display: none;");
         phaseBool = false;
     }
-}
-
-function showCountdown(phase, count) {
-    console.log('Transitioning to countdown phase');
-
-    if (count == 0) {
-        // set up html and css
-        const phase_div = document.createElement("div");
-        phase_div.setAttribute("id", "phase_modal");
-        phase_div.setAttribute("style", "display: block; position: fixed;" +
-            " z-index: 20; left: 0; top: 0; width: 100%; height: 100%; " +
-            "background-color: rgba(0, 0, 0, 0.2); overflow: hidden; transition: 1.5s; font-size: 16px");
-
-        let phase_innerHTML = "";
-
-        phase_innerHTML += `<img src="${phase.banner}" id="banner"/>`;
-
-        if (phase.text.some(line => line !== "")) {
-            phase_innerHTML += `<div id="banner_text_box">`;
-            phase.text.forEach((line) => {
-                phase_innerHTML += `<span class="info">${line}</span>`;
-            });
-            phase_innerHTML += `</div>`;
-        }
-
-        phase_innerHTML += ``;
-        phase_div.innerHTML = phase_innerHTML;
-        document.body.appendChild(phase_div);
-    }
-    else {
-        let phase_innerHTML = "";
-
-        phase_innerHTML += `<img src="${phase.banner}" id="banner"/>`;
-
-        if (phase.text.some(line => line !== "")) {
-            phase_innerHTML += `<div id="banner_text_box">`;
-            phase.text.forEach((line) => {
-                phase_innerHTML += `<span class="info">${line}</span>`;
-            });
-            phase_innerHTML += `</div>`;
-        }
-
-        phase_innerHTML += ``;
-
-        document.getElementById("phase_modal").innerHTML = phase_innerHTML;
-    }
-
-    document.getElementById("banner").setAttribute("style",
-        "background-color: transparent; width: calc(30vw + 15vh); height: auto; border-radius: 12px;" +
-        " position: absolute; top: 70%; left: 50%;" +
-        " z-index: 5; transition: 1.5s ease-in-out; transform: translate(-50%, -50%);");
-
-    if (phase.text.some(line => line !== "")) {
-        const text = document.getElementById("banner_text_box");
-        text.setAttribute("style", " display: flex; flex-direction: column; position: absolute;" +
-            " top: 80%; left: 43%; transform: translate(-50%, -50%);" +
-            " color: #C9FFFC; font-size: 2rem; font-family: 'Comfortaa', Arial, sans-serif; text-align: center;" +
-            " z-index: 10; padding: 10px 20px; border-radius: 8px; transform: translate(-50%, -50%);");
-    }
-
-    var infos = document.getElementsByClassName("info");
-    for (var i = 0; i < infos.length; i++) {
-        // infos[i].setAttribute("style", "text-align: center; font-size: calc(0.045 * 40vh);" +
-        //     " z-index: 21; transition: 1.5s east-in;");
-        infos[i].setAttribute("style", "text-align: center; font-size: calc(0.03 * 40vh);" +
-            " z-index: 21; transition: 1.5s east-in;");
-    }
-
-    // clear phase after 20 seconds
-    if (count == 19) {
-        setTimeout(() => {
-            document.getElementById("phase_modal").remove();
-        }, 1000);
-    }
-
-// else {
-//     // Hide the current phase modal if it's already showing
-//     document.getElementById("phase_modal").setAttribute("style", "display: none;");
-//     phaseBool = false;
-// }
 }
