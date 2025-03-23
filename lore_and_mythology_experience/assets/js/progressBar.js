1eb59ccdca50cb93cc5fac9666e9f6a10d6bc96d
Ryan McManamy <ramcmana@asu.edu>
2025-03-23 01:24:40 -0700

    initialize instead of contructor

--- lore_and_mythology_experience/assets/js/progressBar.js
+++ lore_and_mythology_experience/assets/js/progressBar.js
@@ -1,13 +1,27 @@
 export default class ProgressBar {
+	progress;
+	progressBar;
+	progressBars;
 
 	constructor(currentProgress) {
 		this.progress = currentProgress;
+		// this.progressBar = document.createElement("div");
+  //       this.progressBar.setAttribute("id", "progressBar");
+  //       this.progressBar.setAttribute("style", "width: 100%; height: 20px; background-color: rgba(200, 200, 200, 0.2); top: calc(100vh - 20px)");
+  //       this.progressBars = document.createElement("div");
+  //       this.progressBars.setAttribute("id", "progressBars");
+  //       this.progressBars.setAttribute("style", "width: calc(progress * 10vw); height: 20px; background-color: rgba(0, 200, 0, 0.2); top: calc(100vh - 20px)");
+  //       document.body.appendChild(progressBar);
+  //       document.body.appendChild(progressBars);
+	}
+
+	initialize() {
 		this.progressBar = document.createElement("div");
-        this.progressBar.setAttribute("id", "progressBar");
-        this.progressBar.setAttribute("style", "width: 100%; height: 20px; background-color: rgba(200, 200, 200, 0.2); top: calc(100vh - 20px)");
+        progressBar.setAttribute("id", "progressBar");
+        progressBar.setAttribute("style", "width: 100%; height: 20px; background-color: rgba(200, 200, 200, 0.2); top: calc(100vh - 20px)");
         this.progressBars = document.createElement("div");
-        this.progressBars.setAttribute("id", "progressBars");
-        this.progressBars.setAttribute("style", "width: calc(progress * 10vw); height: 20px; background-color: rgba(0, 200, 0, 0.2); top: calc(100vh - 20px)");
+        progressBars.setAttribute("id", "progressBars");
+        progressBars.setAttribute("style", "width: calc(progress * 10vw); height: 20px; background-color: rgba(0, 200, 0, 0.2); top: calc(100vh - 20px)");
         document.body.appendChild(progressBar);
         document.body.appendChild(progressBars);
 	}
