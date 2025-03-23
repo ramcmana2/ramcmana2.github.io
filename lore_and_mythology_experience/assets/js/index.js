e9bb8165fbccddea8c34e17d417b3ed9b50d3764
Ryan McManamy <ramcmana@asu.edu>
2025-03-23 01:46:51 -0700

    Weird bug, but ok

--- lore_and_mythology_experience/assets/js/index.js
+++ lore_and_mythology_experience/assets/js/index.js
@@ -1,8 +1,16 @@
 import ProgressBar from './progressBar.js';
 
 const pBar = new ProgressBar(0);
+pBar.initialize();
 
-function onClick() {
+// function onClick() {
+//     let iframe = document.createElement('iframe');
+//     iframe.style = "position: fixed; top: 0px; bottom: 0px; right: 0px; width: 100%; border: none; margin: 0; padding: 0; overflow: hidden; z-index: 999999; height: 100%;";
+//     iframe.src = "../pages/amp_context.html";
+//     document.body.appendChild(iframe);
+// }
+
+window.onClick = function() {
     let iframe = document.createElement('iframe');
     iframe.style = "position: fixed; top: 0px; bottom: 0px; right: 0px; width: 100%; border: none; margin: 0; padding: 0; overflow: hidden; z-index: 999999; height: 100%;";
     iframe.src = "../pages/amp_context.html";
