fd3d59abe61b282e0685f722e534daed695d3248
Ryan McManamy <ramcmana@asu.edu>
2025-03-23 01:50:02 -0700

    this.everything

--- lore_and_mythology_experience/assets/js/progressBar.js
+++ lore_and_mythology_experience/assets/js/progressBar.js
@@ -17,13 +17,13 @@ export default class ProgressBar {
 
 	initialize() {
 		this.progressBar = document.createElement("div");
-        progressBar.setAttribute("id", "progressBar");
-        progressBar.setAttribute("style", "width: 100%; height: 20px; background-color: rgba(200, 200, 200, 0.2); top: calc(100vh - 20px)");
+        this.progressBar.setAttribute("id", "progressBar");
+        this.progressBar.setAttribute("style", "width: 100%; height: 20px; background-color: rgba(200, 200, 200, 0.2); top: calc(100vh - 20px)");
         this.progressBars = document.createElement("div");
-        progressBars.setAttribute("id", "progressBars");
-        progressBars.setAttribute("style", "width: calc(progress * 10vw); height: 20px; background-color: rgba(0, 200, 0, 0.2); top: calc(100vh - 20px)");
-        document.body.appendChild(progressBar);
-        document.body.appendChild(progressBars);
+        this.progressBars.setAttribute("id", "progressBars");
+        this.progressBars.setAttribute("style", "width: calc(progress * 10vw); height: 20px; background-color: rgba(0, 200, 0, 0.2); top: calc(100vh - 20px)");
+        document.body.appendChild(this.progressBar);
+        document.body.appendChild(this.progressBars);
 	}
 
 	drawProgressBar() {
