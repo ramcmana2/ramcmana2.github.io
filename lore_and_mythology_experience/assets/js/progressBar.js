4a2ecb8035e68254aaf305714b4e7c9a2bc544ca
Ryan McManamy <ramcmana@asu.edu>
2025-03-26 14:54:08 -0700

    changed 100% to 100vw

--- lore_and_mythology_experience/assets/js/progressBar.js
+++ lore_and_mythology_experience/assets/js/progressBar.js
@@ -84,7 +84,7 @@ export default function testIframe(currentProgress) {
 
 	let progressBar = document.createElement("div");
     progressBar.setAttribute("id", "progressBar");
-    progressBar.setAttribute("style", "width: 100%; height: 20px; background-color: rgba(200, 200, 200, 0.2); top: calc(100vh - 20px); z-index: 100; display: block;");
+    progressBar.setAttribute("style", "width: 100vw; height: 20px; background-color: rgba(200, 200, 200, 0.2); top: calc(100vh - 20px); z-index: 100; display: block;");
     let progressBars = document.createElement("div");
     progressBars.setAttribute("id", "progressBars");
     progressBars.setAttribute("style", "width: calc(progress * 10vw); height: 20px; background-color: rgba(0, 200, 0, 0.2); top: calc(100vh - 20px); z-index: 100; display: block;");
