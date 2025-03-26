83bac54d1ff8726b8826719689760cf05a02fed3
Ryan McManamy <ramcmana@asu.edu>
2025-03-26 12:44:28 -0700

    display: block;

--- lore_and_mythology_experience/assets/js/progressBar.js
+++ lore_and_mythology_experience/assets/js/progressBar.js
@@ -18,10 +18,10 @@ export default class ProgressBar {
 	initialize() {
 		this.progressBar = document.createElement("div");
         this.progressBar.setAttribute("id", "progressBar");
-        this.progressBar.setAttribute("style", "width: 100%; height: 20px; background-color: rgba(200, 200, 200, 0.2); position: absolute; top: calc(100vh - 20px); z-index: 100;");
+        this.progressBar.setAttribute("style", "width: 100%; height: 20px; background-color: rgba(200, 200, 200, 0.2); position: absolute; top: calc(100vh - 20px); z-index: 100; display: block;");
         this.progressBars = document.createElement("div");
         this.progressBars.setAttribute("id", "progressBars");
-        this.progressBars.setAttribute("style", "width: calc(progress * 10vw); height: 20px; background-color: rgba(0, 200, 0, 0.2); position: absolute; top: calc(100vh - 20px); z-index: 100;");
+        this.progressBars.setAttribute("style", "width: calc(progress * 10vw); height: 20px; background-color: rgba(0, 200, 0, 0.2); position: absolute; top: calc(100vh - 20px); z-index: 100; display: block;");
         document.body.appendChild(this.progressBar);
         document.body.appendChild(this.progressBars);
 	}
@@ -31,6 +31,6 @@ export default class ProgressBar {
 		// for (var i = 0; i < this.progress; i++) {
 		// 	this.progressBars.setAttribute("style", "width: calc(progress * 10vw); height: 20px; background-color: rgba(0, 200, 0, 0.2);");
 		// }
-		this.progressBars.setAttribute("style", "width: calc(progress * 10vw); height: 20px; background-color: rgba(0, 200, 0, 0.2); position: absolute; top: calc(100vh - 20px); z-index: 100;");
+		this.progressBars.setAttribute("style", "width: calc(progress * 10vw); height: 20px; background-color: rgba(0, 200, 0, 0.2); position: absolute; top: calc(100vh - 20px); z-index: 100; display: block;");
 	}
 }
