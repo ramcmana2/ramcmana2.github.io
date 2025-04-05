297fb0e1f43bb6b1a6d68befd4c4da48c3399c16
Ryan McManamy <ramcmana@asu.edu>
2025-04-04 18:19:45 -0700

    Endless css part0

--- lore_and_mythology_experience/assets/js/phasesSMP.js
+++ lore_and_mythology_experience/assets/js/phasesSMP.js
@@ -107,10 +107,10 @@ export function startPhasesSMP(phasesAudioManager) {
     //document.getElementById("close_help").setAttribute("style", "position: absolute; top: calc(100vh - 70vh - ((0.09 * 0.7 * 70vh) / 2)); left: calc((100vw / 2) + ((0.8 * 70vh) / 4)); font-family: Arial, Helvetica, sans-serif; color: #333; font-size: calc(0.09 * 0.7 * 70vh); cursor: pointer;");
     document.getElementById("close_help").textContent = "×";
     //document.getElementById("close_help").setAttribute("style", "position: absolute; top: calc(((100vh - 70vh) / 2) + (0.07 * 0.7 * 70vh)); left: calc((100vw / 2) + (70vh / 4)); font-family: Arial, Helvetica, sans-serif; color: #333; font-size: calc(0.09 * 0.7 * 70vh); cursor: pointer;");
-    document.getElementById("close_help").setAttribute("style", "position: absolute; top: calc(((100vh - 70vh) / 2) + (0.07 * 0.7 * 70vh)); left: calc((100vw / 2) + ((0.7 * 70vh) / 4)); font-family: Arial, Helvetica, sans-serif; font-size: 30px; font-weight: bold; cursor: pointer;");
+    document.getElementById("close_help").setAttribute("style", "position: absolute; top: calc(((100vh - 70vh) / 2) + (0.07 * 0.7 * 70vh)); left: calc((100vw / 2) + ((0.7 * 70vh) / 3.5)); font-family: Arial, Helvetica, sans-serif; font-size: 30px; font-weight: bold; cursor: pointer;");
     //document.getElementById("help_modal_title").setAttribute("style", "position: absolute; top: calc(100vh - 70vh + ((0.11 * 0.7 * 70vh) / 2)); left: calc((100vw / 2) - (4 * 0.35 * 0.11 * 0.7 * 70vh)); font-family: Arial, Helvetica, sans-serif; color: #333; font-size: calc(0.11 * 0.7 * 70vh);");
     //document.getElementById("help_modal_title").setAttribute("style", "position: absolute; top: calc(((100vh - 70vh) / 2) + (0.11 * 0.7 * 70vh)); left: calc((100vw / 2) - (2.7 * 0.35 * 0.11 * 0.7 * 70vh)); font-family: Arial, Helvetica, sans-serif; color: #333; font-size: calc(0.11 * 0.7 * 70vh);");
-    document.getElementById("help_modal_title").setAttribute("style", "position: absolute; top: calc(((100vh - 70vh) / 2) + (0.1 * 0.7 * 70vh)); left: calc((100vw / 2) - (0.055 * 0.7 * 70vh)); font-family: Arial, Helvetica, sans-serif; font-size: 24px;");
+    document.getElementById("help_modal_title").setAttribute("style", "position: absolute; top: calc(((100vh - 70vh) / 2) + (0.1 * 0.7 * 70vh)); left: calc((100vw / 2) - (0.058 * 0.7 * 70vh)); font-family: Arial, Helvetica, sans-serif; font-size: 24px;");
     var swipes = document.getElementsByClassName("swipe");
     for (var i = 0; i < swipes.length; i++) {
         //swipes[i].setAttribute("style", "position: absolute; top: calc(3.3 * (100vh - 70vh)); left: calc((100vw / 2) - (0.7 * 70vh / 2) + (0.22 * 0.7 * 70vh)); display: flex; flex-direction: column; font-family: Arial, Helvetica, sans-serif; color: #333;");
@@ -120,7 +120,7 @@ export function startPhasesSMP(phasesAudioManager) {
     var taps = document.getElementsByClassName("tap");
     for (var j = 0; j < taps.length; j++) {
         //taps[i].setAttribute("style", "position: absolute; top: calc(3.17 * (100vh - 70vh)); left: calc((100vw / 2) - (0.1 * 0.7 * 70vh)); display: flex; flex-direction: column; font-family: Arial, Helvetica, sans-serif; color: #333;");
-        taps[j].setAttribute("style", "position: absolute; top: calc(3 * (100vh - 70vh)); left: calc((100vw / 2) - (0.25 * 0.7 * 70vh)); font-family: Arial, Helvetica, sans-serif;");
+        taps[j].setAttribute("style", "position: absolute; top: calc(1.2 * (100vh - 70vh)); left: calc((100vw / 2) - (0.25 * 0.7 * 70vh)); font-family: Arial, Helvetica, sans-serif;");
     }
     //document.getElementById("swipe").setAttribute("style", "font-family: Arial, Helvetica, sans-serif; color: #333;");
     document.getElementById("tap").setAttribute("style", "width: calc(0.3 * 0.7 * 70vh); align-self: center; padding: 0.99vh;");
