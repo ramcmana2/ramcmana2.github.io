8aefba9bb8d6df9c8020e7813dfffdc079fea7bb
Ryan McManamy <ramcmana@asu.edu>
2025-04-21 20:58:19 -0700

    test2

--- lore_and_mythology_experience/assets/js/interactivityTest.js
+++ lore_and_mythology_experience/assets/js/interactivityTest.js
@@ -8,7 +8,7 @@ export default function interactivityTest() {
 
 	const width = window.screen.width;
 	const height = window.screen.height;
-	const originalDisplay = window.canvas.getImageData(0, 0, width, height);
+	// const originalDisplay = window.canvas.getImageData(0, 0, width, height);
 
 	let different = false;
 
@@ -16,16 +16,28 @@ export default function interactivityTest() {
 		let randomX = Math.floor(Math.random() * (width + 1));
 		let randomY = Math.floor(Math.random() * (height + 1));
 
+		let oldColor = document.elementFromPoint(randomX, randomY).style.backgroundColor();
+
+		randomX = Math.floor(Math.random() * (width + 1));
+		randomY = Math.floor(Math.random() * (height + 1));
+
 		document.elementFromPoint(randomX, randomY).click();
 
-		const newDisplay = window.canvas.getImageData(0, 0, width, height);
+		let newColor = document.elementFromPoint(randomX, randomY).style.backgroundColor();
 
-		for (var i = 0; i < width * height * 4; i++) {
-			if (newDisplay.data[i] != originalDisplay.data[i]) {
-				different = true;
-				break;
-			}
+		// const newDisplay = window.canvas.getImageData(0, 0, width, height);
+
+		if (newColor != oldColor) {
+			different = true;
+			break;
 		}
+
+		// for (var i = 0; i < width * height * 4; i++) {
+		// 	if (newDisplay.data[i] != originalDisplay.data[i]) {
+		// 		different = true;
+		// 		break;
+		// 	}
+		// }
 	}
 
 	console.log("Interactivity confirmed.");
