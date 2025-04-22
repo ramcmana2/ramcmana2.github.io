67bfc091b4ed709f77fa176db943cf09c57c23fc
Ryan McManamy <ramcmana@asu.edu>
2025-04-21 21:10:02 -0700

    test4

--- lore_and_mythology_experience/assets/js/interactivityTest.js
+++ lore_and_mythology_experience/assets/js/interactivityTest.js
@@ -16,18 +16,26 @@ export default function interactivityTest() {
 		let randomX = Math.floor(Math.random() * (width + 1));
 		let randomY = Math.floor(Math.random() * (height + 1));
 
-		let oldColor = document.elementFromPoint(randomX, randomY).style.backgroundColor;
+		// let oldColor = document.elementFromPoint(randomX, randomY).style.backgroundColor;
+		let oldColor = document.elementFromPoint(randomX, randomY);
+		if (oldColor != null) {
+			oldColor = oldColor.style.backgroundColor;
+		}
 
 		randomX = Math.floor(Math.random() * (width + 1));
 		randomY = Math.floor(Math.random() * (height + 1));
 
 		document.elementFromPoint(randomX, randomY).click();
 
-		let newColor = document.elementFromPoint(randomX, randomY).style.backgroundColor;
+		// let newColor = document.elementFromPoint(randomX, randomY).style.backgroundColor;
+		let newColor = document.elementFromPoint(randomX, randomY);
+		if (newColor != null) {
+			newColor = newColor.style.backgroundColor;
+		}
 
 		// const newDisplay = window.canvas.getImageData(0, 0, width, height);
 
-		if (newColor != oldColor) {
+		if (newColor != null && oldColor != null && newColor != oldColor) {
 			different = true;
 			break;
 		}
