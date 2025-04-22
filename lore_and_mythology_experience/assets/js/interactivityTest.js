12de594187963f284e943d6afd05d581f28c8b65
Ryan McManamy <ramcmana@asu.edu>
2025-04-21 23:52:55 -0700

    test22

--- lore_and_mythology_experience/assets/js/interactivityTest.js
+++ lore_and_mythology_experience/assets/js/interactivityTest.js
@@ -4,75 +4,15 @@
  * Only applicable to screens without animations.
  */
 export default async function interactivityTest(intervalID) {
-	// console.log("Initiating automatic interactivity test... Please wait.");
-
-	// const width = window.screen.width;
-	// const height = window.screen.height;
-	// // const originalDisplay = window.canvas.getImageData(0, 0, width, height);
-
-	// let different = false;
-
-	// while (!different) {
-	// 	let randomX1 = Math.floor(Math.random() * (width + 1));
-	// 	let randomY1 = Math.floor(Math.random() * (height + 1));
-
-	// 	// let oldColor = document.elementFromPoint(randomX, randomY).style.backgroundColor;
-	// 	let oldColor = document.elementFromPoint(randomX1, randomY1);
-	// 	if (oldColor != null) {
-	// 		oldColor = oldColor.style.backgroundColor;
-	// 	}
-
-	// 	setTimeout(() => {
-	// 		let randomX2 = Math.floor(Math.random() * (width + 1));
-	// 		let randomY2 = Math.floor(Math.random() * (height + 1));
-
-	// 		// document.elementFromPoint(randomX, randomY).click();
-	// 		let randomElement = document.elementFromPoint(randomX2, randomY2);
-	// 		if (randomElement != null) {
-	// 			randomElement.click();
-	// 		}
-
-	// 		// let newColor = document.elementFromPoint(randomX, randomY).style.backgroundColor;
-	// 		let newColor = document.elementFromPoint(randomX1, randomY1);
-	// 		if (newColor != null) {
-	// 			newColor = newColor.style.backgroundColor;
-	// 		}
-
-	// 		// const newDisplay = window.canvas.getImageData(0, 0, width, height);
-
-	// 		if (newColor != null && oldColor != null && newColor != oldColor) {
-	// 			different = true;
-	// 			clearInterval(intervalID);
-	// 			//break;
-	// 		}
-	// 		else {
-	// 			console.log("No change.");
-	// 		}
-
-	// 		// for (var i = 0; i < width * height * 4; i++) {
-	// 		// 	if (newDisplay.data[i] != originalDisplay.data[i]) {
-	// 		// 		different = true;
-	// 		// 		break;
-	// 		// 	}
-	// 		// }
-	//     }, 2500);
-	// }
-
-	// console.log("Interactivity confirmed.");
-
 	console.log("Initiating automatic interactivity test... Please wait.");
 
 	const width = window.screen.width;
 	const height = window.screen.height;
+	console.log("width: " + width);
+	console.log("height: " + height);
 
-	// let randomX1 = Math.floor(Math.random() * (width + 1));
-	// let randomY1 = Math.floor(Math.random() * (height + 1));
-	// let randomX1 = 950;
-	// let randomY1 = 300;
-	// let randomX1 = 1877;
-	// let randomY1 = 613;
-	let randomX1 = 948;
-	let randomY1 = 264;
+	let randomX1 = width / 2;
+	let randomY1 = height / 2;
 
 	let oldColor = document.elementFromPoint(randomX1, randomY1);
 	if (oldColor != null) {
@@ -80,22 +20,9 @@ export default async function interactivityTest(intervalID) {
 	}
 
 	setTimeout(() => {
-		// let randomX2 = Math.floor(Math.random() * (width + 1));
-		// let randomY2 = Math.floor(Math.random() * (height + 1));
-		// let randomX2 = 140;
-		// let randomY2 = 600;
-		// let randomX2 = 1877;
-		// let randomY2 = 613;
-		let randomX2 = 1879;
-		let randomY2 = 528;
+		let randomX2 = width - 25;
+		let randomY2 = height - 65;
 
-		console.log("x = " + randomX2);
-		console.log("y = " + randomY2);
-
-		// let randomElement = document.elementFromPoint(randomX2, randomY2);
-		// if (randomElement != null) {
-		// 	randomElement.click();
-		// }
 		let elements = document.elementsFromPoint(randomX2, randomY2);
 		if (elements != null) {
 			elements.forEach((elem, i) => {
@@ -110,7 +37,7 @@ export default async function interactivityTest(intervalID) {
 			newColor = newColor.style.backgroundColor;
 		}
 
-		if (newColor != null && oldColor != null && newColor != oldColor) {
+		if (newColor != oldColor || (newColor != null && oldColor == null) || (oldColor != null && newColor == null)) {
 			console.log("Interactivity confirmed.");
 			clearInterval(intervalID);
 		}
