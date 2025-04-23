933c955e96096f4f9867df528148a75367357095
Ryan McManamy <ramcmana@asu.edu>
2025-04-22 18:48:51 -0700

    test38

--- lore_and_mythology_experience/assets/js/interactivityTest.js
+++ lore_and_mythology_experience/assets/js/interactivityTest.js
@@ -13,30 +13,31 @@ export default async function interactivityTest(intervalID) {
 	let randomY1 = Math.floor(height / 2);
 	// let randomX1 = Math.floor(Math.random() * (width + 1));
 	// let randomY1 = Math.floor(Math.random() * (height + 1));
-	console.log("randomX1: " + randomX1);
-	console.log("randomY1: " + randomY1);
+	// console.log("randomX1: " + randomX1);
+	// console.log("randomY1: " + randomY1);
 
-	let oldColors = document.elementsFromPoint(randomX1, randomY1);
-	console.log("oldColors: " + oldColors);
-	let oldColor = oldColors[0];
-	console.log("oldColor: " + oldColor);
-	if (oldColors != null) {
-		for (var elem = 0; elem < oldColors.length; elem++) {
-			if (oldColors[elem].style.backgroundColor != null) {
-				console.log("oldColors[elem].style.backgroundColor = " + oldColors[elem].style.backgroundColor);
-				oldColor = oldColors[elem].style.backgroundColor;
-				break;
-			}
-		}
-	}
-	console.log("oldColor: " + oldColor);
+	// let oldColors = document.elementsFromPoint(randomX1, randomY1);
+	// console.log("oldColors: " + oldColors);
+	// let oldColor = oldColors[0];
+	// console.log("oldColor: " + oldColor);
+	// if (oldColors != null) {
+	// 	for (var elem = 0; elem < oldColors.length; elem++) {
+	// 		if (oldColors[elem].style.backgroundColor != null) {
+	// 			console.log("oldColors[elem].style.backgroundColor = " + oldColors[elem].style.backgroundColor);
+	// 			oldColor = oldColors[elem].style.backgroundColor;
+	// 			break;
+	// 		}
+	// 	}
+	// }
+	// console.log("oldColor: " + oldColor);
+	let oldLayers = document.elementsFromPoint(randomX1, randomY1);
 
 	let randomX2 = width - 25;
 	let randomY2 = height - 65;
 	// let randomX2 = Math.floor(Math.random() * (width + 1));
 	// let randomY2 = Math.floor(Math.random() * (height + 1));
-	console.log("randomX2: " + randomX2);
-	console.log("randomY2: " + randomY2);
+	// console.log("randomX2: " + randomX2);
+	// console.log("randomY2: " + randomY2);
 
 	let elements = document.elementsFromPoint(randomX2, randomY2);
 	if (elements != null) {
@@ -62,30 +63,38 @@ export default async function interactivityTest(intervalID) {
 		// 	});
 		// }
 
-		let newColors = document.elementsFromPoint(randomX1, randomY1);
-		console.log("newColors: " + newColors);
-		let newColor = newColors[0];
-		console.log("newColor: " + newColor);
-		if (newColors != null) {
-			for (var elem = 0; elem < newColors.length; elem++) {
-				if (newColors[elem].style.backgroundColor != null) {
-					console.log("newColors[elem].style.backgroundColor = " + newColors[elem].style.backgroundColor);
-					newColor = newColors[elem].style.backgroundColor;
-					break;
-				}
-			}
-		}
-		console.log("newColor: " + newColor);
+		// let newColors = document.elementsFromPoint(randomX1, randomY1);
+		// console.log("newColors: " + newColors);
+		// let newColor = newColors[0];
+		// console.log("newColor: " + newColor);
+		// if (newColors != null) {
+		// 	for (var elem = 0; elem < newColors.length; elem++) {
+		// 		if (newColors[elem].style.backgroundColor != null) {
+		// 			console.log("newColors[elem].style.backgroundColor = " + newColors[elem].style.backgroundColor);
+		// 			newColor = newColors[elem].style.backgroundColor;
+		// 			break;
+		// 		}
+		// 	}
+		// }
+		// console.log("newColor: " + newColor);
+		let newLayers = document.elementsFromPoint(randomX1, randomY1);
 
-		if (newColor != oldColor || (newColor != null && oldColor == null) || (oldColor != null && newColor == null)) {
-			console.log("oldColor: " + oldColor);
-			console.log("newColor: " + newColor);
+		// if (newColor != oldColor || (newColor != null && oldColor == null) || (oldColor != null && newColor == null)) {
+		// 	console.log("oldColor: " + oldColor);
+		// 	console.log("newColor: " + newColor);
+		// 	console.log("Interactivity confirmed.");
+		// 	clearInterval(intervalID);
+		// }
+		// else {
+		// 	console.log("oldColor: " + oldColor);
+		// 	console.log("newColor: " + newColor);
+		// 	console.log("No change.");
+		// }
+		if (newLayers.length != oldLayers.length) {
 			console.log("Interactivity confirmed.");
 			clearInterval(intervalID);
 		}
 		else {
-			console.log("oldColor: " + oldColor);
-			console.log("newColor: " + newColor);
 			console.log("No change.");
 		}
 	}, 2500);
